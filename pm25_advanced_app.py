73dc8b28f501bbd021203773d5242bf774165c78
Liu Kuang Ting <31066104+timo456@users.noreply.github.com>
2025-05-27 23:36:36 +0800

    Update pm25_advanced_app.py

--- pm25_advanced_app.py
+++ pm25_advanced_app.py
@@ -121,14 +121,13 @@ with tab2:
             st.markdown(f"- Laplacian 標準差：{lap_std:.2f}")
             st.markdown(f"- Sobel 邊緣強度：{sobel:.2f}")
             st.markdown(f"- 平均亮度：{brightness:.2f}")
-            st.info(f"🔍 判斷：{result}")
-            st.info(f"🔍 判斷：{result}")
+           st.info(f"🔍 判斷：{result}")
             if pm25 == "未超標":
-                st.success(f"🔍 判斷：{result}｜✅ PM2.5 狀態：未超標")
+                st.success("✅ PM2.5 狀態：未超標")
             elif pm25 == "超標":
-                st.error(f"🔍 判斷：{result}｜❌ PM2.5 狀態：超標")
+                st.error("❌ PM2.5 狀態：超標")
             else:
-                st.info(f"🔍 判斷：{result}｜⚠️ PM2.5 狀態：無法判斷")
+                st.info("⚠️ PM2.5 狀態：無法判斷")
             results.append({
                 "檔名": file.name,
                 "藍天比例(%)": blue,
